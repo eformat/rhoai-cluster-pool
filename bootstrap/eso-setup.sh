4235cef39a626e1281294587f0535821d233bb6f
Mike Hepburn <eformat@gmail.com>
2025-11-10 18:49:39 +1000

    🏺 add ESO 🏺

--- bootstrap/eso-setup.sh
+++ bootstrap/eso-setup.sh
@@ -34,22 +34,45 @@ patch_proxy() {
 }
 patch_proxy
 
+eso_configmap() {
+    echo "🌴 Running eso_configmap..."
+
+cat <<EOF | oc apply -f-
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: ca-bundle
+  namespace: external-secrets
+  labels:
+    config.openshift.io/inject-trusted-cabundle: 'true'
+EOF
+
+    if [ "$?" != 0 ]; then
+        echo -e "🚨${RED}Failed - to run eso_configmap ?${NC}"
+        exit 1
+    else
+        echo "🌴 eso_configmap ran OK"
+    fi
+}
+eso_configmap
+
 check_done() {
     echo "🌴 Running check_done..."
     STATUS=$(oc -n external-secrets get $(oc get pods -n external-secrets -l app.kubernetes.io/name=external-secrets -o name) -o=jsonpath='{.status.conditions[?(@.type=="Ready")].status}')
-    if [ "$STATUS" != "True" ]; then
-      echo -e "💀${ORANGE}Warn - check_done not ready for eso, continuing ${NC}"
-      return 1
-    else
-      echo "🌴 check_done ran OK"
-    fi
+    until [ "$STATUS" == "True" ]
+    do
+        echo -e "${GREEN}Waiting for eso pod to be ready.${NC}"
+        sleep 5
+        ((i=i+1))
+        if [ $i -gt 100 ]; then
+            echo -e "🚨${RED}Failed waiting for eso pod to be ready?.${NC}"
+            exit 1
+        fi
+        STATUS=$(oc -n external-secrets get $(oc get pods -n external-secrets -l app.kubernetes.io/name=external-secrets -o name) -o=jsonpath='{.status.conditions[?(@.type=="Ready")].status}')
+    done
+    echo "🌴 check_done ran OK"
     return 0
 }
-
-if check_done; then
-    echo -e "\n🌻${GREEN}ESO setup OK.${NC}🌻\n"
-    exit 0;
-fi
 
 apply_helm() {
     echo "🌴 Apply ESO Helm..."
@@ -77,6 +100,31 @@ apply_helm() {
     echo "🌴 Apply ESO Helm Done"
 }
 apply_helm
+
+apply_cr() {
+    echo "🌴 Apply ESO CR..."
+
+    wget -P /tmp https://raw.githubusercontent.com/eformat/rhoai-cluster-pool/refs/heads/main/bootstrap/external-secrets-cr.yaml
+    if [ ! -f "/tmp/external-secrets-cr.yaml" ]; then
+        echo -e "🕱${RED}Failed - to get external-secrets-cr file ?${NC}"
+        exit 1
+    fi
+
+    cat /tmp/external-secrets-cr.yaml | envsubst | oc apply -f-
+    until [ "${PIPESTATUS[2]}" == 0 ]
+    do
+        echo -e "${GREEN}Waiting for 0 rc from oc commands.${NC}"
+        ((i=i+1))
+        if [ $i -gt 10 ]; then
+            echo -e "🕱${RED}Failed - eso cr never ready?.${NC}"
+            exit 1
+        fi
+        sleep 10
+        cat /tmp/external-secrets-cr.yaml | envsubst | oc apply -f-
+    done
+    echo "🌴 Apply ESO CR Done"
+}
+apply_cr
 
 if check_done; then
     echo -e "\n🌻${GREEN}ESO setup OK.${NC}🌻\n"
