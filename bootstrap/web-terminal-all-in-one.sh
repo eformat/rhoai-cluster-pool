193631d9263745f5e4ea59dd0aba54c4a8351178
Mike Hepburn <eformat@gmail.com>
2025-11-25 18:44:02 +1000

    🗡️ RHOAI 3.0.0 - web-terminal-all-in-one 🗡️

---
+++ bootstrap/web-terminal-all-in-one.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+oc apply -f applications/web-terminal/web-terminal-all-in-one.yaml
+oc wait --for=condition=Ready pod -l app.kubernetes.io/name=web-terminal-controller -n openshift-operators --timeout=600s
+oc apply -f applications/web-terminal/web-terminal-all-in-one.yaml
+echo "🌴 web-terminal-all-in-one installed"
